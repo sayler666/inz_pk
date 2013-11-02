ef482828fa1fd25391d1af07b4412a9a246980f2
sayler <lukasz.chromy@gmail.com>
2013-11-02 22:03:01 +0100

    update adt
    include g play service

--- inz/gen/android/support/v4/preferencefragment/R.java
+++ inz/gen/android/support/v4/preferencefragment/R.java
@@ -8,24 +8,24 @@ package android.support.v4.preferencefragment;
 
 public final class R {
 	public static final class dimen {
-		public static final int preference_fragment_padding_bottom = 0x7f060000;
-		public static final int preference_fragment_padding_side = 0x7f060001;
+		public static final int preference_fragment_padding_bottom = 0x7f090000;
+		public static final int preference_fragment_padding_side = 0x7f090001;
 	}
 	public static final class id {
-		public static final int back_button = 0x7f0a004c;
-		public static final int button_bar = 0x7f0a004b;
-		public static final int next_button = 0x7f0a004e;
-		public static final int skip_button = 0x7f0a004d;
+		public static final int back_button = 0x7f07004c;
+		public static final int button_bar = 0x7f07004b;
+		public static final int next_button = 0x7f07004e;
+		public static final int skip_button = 0x7f07004d;
 	}
 	public static final class integer {
-		public static final int preference_fragment_scrollbarStyle = 0x7f070000;
+		public static final int preference_fragment_scrollbarStyle = 0x7f0a0000;
 	}
 	public static final class layout {
 		public static final int preference_list_fragment = 0x7f03001b;
 	}
 	public static final class string {
-		public static final int back_button_label = 0x7f080000;
-		public static final int next_button_label = 0x7f080001;
-		public static final int skip_button_label = 0x7f080002;
+		public static final int back_button_label = 0x7f080019;
+		public static final int next_button_label = 0x7f08001a;
+		public static final int skip_button_label = 0x7f08001b;
 	}
 }
